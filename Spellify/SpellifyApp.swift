e67b0b5e72348ad4e693d3a365f3308999f7d302
Dror Lapidot <drorlp@gmail.com>
2026-01-04 21:25:53 +0200

    Initial Commit

---
+++ Spellify/SpellifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellifyApp.swift
+//  Spellify
+//
+//  Created by Dror Lapidot on 04/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpellifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpellifyTests/SpellifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpellifyTests.swift
+//  SpellifyTests
+//
+//  Created by Dror Lapidot on 04/01/2026.
+//
+
+import Testing
+@testable import Spellify
+
+struct SpellifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
