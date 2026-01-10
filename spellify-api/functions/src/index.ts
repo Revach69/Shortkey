2008d56b1f9d8a3e9e137022aa17059a2a2ed065
Dror Lapidot <drorlp@gmail.com>
2026-01-11 00:10:28 +0200

    [api] fix build

--- spellify-api/functions/src/index.ts
+++ spellify-api/functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import { onCall } from 'firebase-functions/v2/https';
 import { defineSecret } from 'firebase-functions/params';
 import * as admin from 'firebase-admin';
 import { registerDeviceHandler, RegisterDeviceRequest } from './handlers/registerDevice';
@@ -10,15 +10,12 @@ admin.initializeApp();
 
 const openaiApiKey = defineSecret(SECRETS.OPENAI_API_KEY);
 
-export const registerDevice = functions
-  .https.onCall((request: functions.https.CallableRequest<RegisterDeviceRequest>) => 
-    registerDeviceHandler(request.data)
-  );
+export const registerDevice = onCall<RegisterDeviceRequest>(
+  (request) => registerDeviceHandler(request.data)
+);
 
-export const transformText = functions
-  .runWith({
-    timeoutSeconds: 30,
-    memory: '256MB',
-    secrets: [openaiApiKey],
-  })
-  .https.onCall(withRequestContext(transformTextHandler));
+export const transformText = onCall({
+  timeoutSeconds: 30,
+  memory: '256MiB',
+  secrets: [openaiApiKey],
+}, withRequestContext(transformTextHandler));
